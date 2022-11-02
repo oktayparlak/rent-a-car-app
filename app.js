acb5d3c6af756a1a09a63b06bcac09de3125783e
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-11-02 11:29:03 +0300

    database configru added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 
 const port = process.env.port || 8081;
+
+/**Connect DB */
+mongoose.connect('mongodb://localhost/rentacar-db').then(() => {
+  console.log('DB Connection Successfuly');
+});
 
 /**Template Engine */
 app.set('view engine', 'ejs');
